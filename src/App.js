6a91291fd71650e512de70251fc959ce55c166eb
Victor Bello <victor@ragnarok>
2017-06-24 16:38:54 -0400

    Se agregaron estilos, imagen de app y otros ajustes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import firebase from 'firebase';
 import FileUpload from './FileUpload';
 
 import './App.css';
+import logo from './familia.png';
 
 class App extends Component {
   constructor(){
     super();
 
     this.state={
-      user:null
+      user:null,
+      percentage:0,
+      pictures:[]
     };
 
     this.handleAuth=this.handleAuth.bind(this);
     this.handleLogout=this.handleLogout.bind(this);
+    this.handleUpload=this.handleUpload.bind(this);
   }
 
   componentWillMount(){
@@ -30,18 +34,40 @@ class App extends Component {
       }
       this.setState({user:userInfo})
     });
+
+    firebase.database().ref('pictures').on('child_added', snapshot=>{
+      this.setState({
+        pictures:this.state.pictures.concat(snapshot.val())
+      })
+    })
   }
 
   renderGoogleButton(){
     if(this.state.user){
-      return(<div>
-                <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName}/>
-                <p>Hola {this.state.user.displayName}!</p>
+      return(<div className="App-intro">
+                <p className="App-intro">¡Hola, { this.state.user.displayName }!</p>
                 <button onClick={this.handleLogout}>Logout</button>
-                <FileUpload/>
+                <FileUpload onUpload={this.handleUpload} percentage={this.state.percentage}/>
+
+                {
+                  this.state.pictures.map(picture=>{
+                   return(
+                     <div className="App-card"  key={Math.random()}>
+                      <figure className="App-card-image">
+                        <img width="320" src={picture.photoURL} alt="Error"/>
+                        <figCaption className="App-card-footer">
+                          <img className="App-card-avatar" src={picture.userImage} alt={picture.userDisplayName} />
+                          <span className="App-card-name">{picture.userDisplayName}</span>
+                        </figCaption>
+                      </figure>
+                    </div>
+                   );
+                  }).reverse()
+                }
+
               </div>);
     }else{
-      return(<button onClick={this.handleAuth}>Login con Google</button>);
+      return(<button onClick={this.handleAuth}>Login width Google</button>);
     }
   }
 
@@ -60,11 +86,41 @@ class App extends Component {
       .catch(error => console.log(`Error ${error.code}:${error.message}`));
   }
 
+  handleUpload(event){
+    const file=event.target.files[0];
+    const storageRef=firebase.storage().ref(`photos/${file.name}`);
+    const task=storageRef.put(file);
+
+   task.on('state_changed', (snapshot) => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      this.setState({
+        percentage:percentage
+      })
+    }, (error) => {
+      console.error(error.message)
+    }, () => {
+      const record={
+        userImage:this.state.user.photoURL,
+        userDisplayName:this.state.user.displayName,
+        photoURL:task.snapshot.downloadURL
+      }
+
+      const dbRef=firebase.database().ref('pictures');
+      const newPicture=dbRef.push();
+      newPicture.set(record);
+
+      this.setState({
+        percentage:0
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Pseudogram</h2>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Familia 2017</h2>
         </div>
         <div className="App-intro">
          {this.renderGoogleButton()}
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,46 +1,15 @@
 import React, {Component} from 'react';
-import firebase from 'firebase';
 
 class FileUpload extends Component{
   constructor(){
     super();
-
-    this.state={
-      uploadValue:0,
-      picture:null
-    }
-
-    this.handleUpload=this.handleUpload.bind(this);
   }
-
-  handleUpload(event){
-    const file=event.target.files[0];
-    const storageRef=firebase.storage().ref(`photos/${file.name}`);
-    const task=storageRef.put(file);
-
-   task.on('state_changed', (snapshot) => {
-      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      this.setState({
-        uploadValue: percentage
-      })
-    }, (error) => {
-      console.error(error.message)
-    }, () => {
-      // Upload complete
-      this.setState({
-        picture: task.snapshot.downloadURL
-      })
-    })
-  }
-
   render(){
     return(
       <div>
-        <progress value={this.state.uploadValue} max="100"></progress>
+        <progress value={this.props.percentage} max="100"></progress>
         <br/>
-        <input type="file" onChange={this.handleUpload}/>
-        <br/>
-        <img width="250" src={this.state.picture} alt=""/>
+        <input type="file" onChange={this.props.onUpload}/>
       </div>
     );
   }
